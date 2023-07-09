ca5efd460d12b540ec097ddd6113e955976e7841
ImperatorNeron <katebarspk@gmail.com>
2023-07-09 13:15:51 +0300

    security settings

--- gruz_resurse/gruz_resurse/wsgi.py
+++ gruz_resurse/gruz_resurse/wsgi.py
@@ -11,10 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from dotenv import load_dotenv
-project_folder = os.path.expanduser('~/prtest')  # adjust as appropriate
-load_dotenv(os.path.join(project_folder, '.env'))
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gruz_resurse.settings")
 
 application = get_wsgi_application()
